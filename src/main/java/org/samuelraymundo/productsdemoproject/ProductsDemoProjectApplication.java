eb797fddb2103e0b85ed8f963051f6f1479fb423
Samuel Raymundo <161658735+SamuelRaymundo@users.noreply.github.com>
2024-08-10 05:05:12 -0300

    Project created

---
+++ src/main/java/org/samuelraymundo/productsdemoproject/ProductsDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package org.samuelraymundo.productsdemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsDemoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductsDemoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/samuelraymundo/productsdemoproject/ProductsDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.samuelraymundo.productsdemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsDemoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
