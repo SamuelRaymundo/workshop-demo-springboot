f577e57c41630839c5197853eca7ac43a3bfdee3
SamuelRaymundo <161658735+SamuelRaymundo@users.noreply.github.com>
2024-08-12 22:59:17 -0300

    User insert and delete

--- src/main/java/org/samuelraymundo/productsdemoproject/services/UserService.java
+++ src/main/java/org/samuelraymundo/productsdemoproject/services/UserService.java
@@ -4,6 +4,7 @@ import org.samuelraymundo.productsdemoproject.entities.User;
 import org.samuelraymundo.productsdemoproject.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,5 +22,13 @@ public class UserService {
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
         return obj.get();
+    }
+
+    public User insert(User user) {
+        return repository.save(user);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
     }
 }
